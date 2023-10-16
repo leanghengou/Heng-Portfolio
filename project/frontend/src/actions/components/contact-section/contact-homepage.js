95e5cdbab078317a259b688641b042ab95968be3
heng <leanghengou5555@gmail.com>
2023-10-15 21:56:56 -0400

    Contact-Homepage

---
+++ project/frontend/src/actions/components/contact-section/contact-homepage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useState } from "react";
+import "./contact-homepage.css"
+import styled from "styled-components"
+
+
+const ContactHomepage = ()=>{
+    return (
+ <div className="contact-homepage-section">
+            <p><span className="highlight-tag-box-style">let's talk!</span></p>
+            <h3>Looking to contact?</h3>
+            <p>We strive to provide the highest quality clothing at the best prices, so you can look your best without breaking the bank.</p>
+        </div>
+
+    )
+}
+
+export default ContactHomepage
--- project/frontend/src/pages/homepage.js
+++ project/frontend/src/pages/homepage.js
@@ -3,6 +3,7 @@ import IntroSection from '../actions/components/introduction-section/intro-secti
 import SkillIcons from "../actions/components/skill-icons-section/skill-icons-section";
 import DisplayScreen from "../actions/components/display-project-screen/project-display-carousel";
 import DevProjectScreen from "../actions/components/development-project-screen/dev-project";
+import ContactHomepage from "../actions/components/contact-section/contact-homepage";
 const Homepage = ()=>{
     return(
         <div>
@@ -10,6 +11,7 @@ const Homepage = ()=>{
         <SkillIcons /> 
         <DisplayScreen/>
         <DevProjectScreen/>
+        <ContactHomepage/>
         </div>
        
     )
